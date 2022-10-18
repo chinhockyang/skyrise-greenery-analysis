85036e8e41f4f9184b4fad8f2cfbf66964cd98f7
chinhockyang <hychycchin@gmail.com>
2022-10-18 17:02:18 +0800

    Match HDB to skyrise greenery

---
+++ 2_hdb_exploration.R
@@ -0,0 +1,58 @@
+# -------------- Load Libraries and Variables --------------
+
+library(rgdal)
+library(dplyr)
+library(tidyr)
+library(sf)
+library(tmap)
+library(ggplot2)
+library(tmaptools)
+# tmaptools::palette_explorer()
+
+library(stringr)
+require(spatstat)
+
+source("constants.R")
+source("1_initial_exploration.R")
+
+# -------------- Prepare HDB dataset --------------
+hdb_info <- read.csv("data/addresses_full.csv")
+
+# handle the 2 missing values
+hdb_info <- hdb_info %>% drop_na(LATITUDE)
+
+# Lowercase Address for Matching
+hdb_info$ADDRESS <- str_to_lower(hdb_info$ADDRESS)
+
+# # Convert to Sf
+sf_hdb <- st_as_sf(hdb_info, coords = c("LONGITUDE", "LATITUDE"), crs= "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+sf_hdb <- st_transform(sf_hdb, crs= 3414)
+
+# -------------- Identify HDB that are Skyrise Greenery --------------
+# lowercase address
+sf_skyrise_greenery$address <- apply(sf_skyrise_greenery, MARGIN=1, FUN = function(x) {
+  address = str_to_lower(x["address"])
+  address = str_replace(address, "avenue", "ave")
+  return(str_trim(str_replace(address, "singapore\\s*\\d*", "")))
+})
+
+# Merge by Postal Code or Address
+# 150
+sf_skyrise_hdb <- sf_hdb %>% filter(
+    (POSTAL_CODE %in% sf_skyrise_greenery$post_code) |
+    (ADDRESS %in% sf_skyrise_greenery$address)
+)
+
+# -------------- Distance Related Skyrise HDB Analysis --------------
+
+qtm(sf_skyrise_hdb)
+
+kf <- Kest(as.ppp(sf_skyrise_hdb$geometry), correction = 'border')
+kf.env <- envelope(as.ppp(sf_skyrise_hdb$geometry),Kest,correction="border")
+plot(kf.env)
+
+lf.env <- envelope(as.ppp(sf_skyrise_hdb$geometry),Lest,correction="border")
+plot(lf.env)
+
+gf.env <- envelope(as.ppp(sf_skyrise_hdb$geometry),Lest,correction="border")
+plot(gf.env)
