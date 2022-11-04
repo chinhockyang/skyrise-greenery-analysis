24e2f97ab00697ed606a8c52347fb4fa6552060c
liangxyan <simliangyan@gmail.com>
2022-11-04 12:12:55 +0800

    tidying csr

--- 6.1_csr_analysis_hdb.R
+++ 6.1_csr_analysis_hdb.R
@@ -9,6 +9,7 @@ library(raster)
 library(units)
 
 source("4_weather_interpolation.R") # to retrieve temp&rainfall interpolation
+source("6_csr_analysis.R") # to re-use the im files for hypo testing
 
 #hdb_prices_with_planning_area <- readOGR("data/hdb_prices_with_planning_area")
 #sf_hdb_prices_with_planning_area <- st_make_valid(st_as_sf(hdb_prices_with_planning_area))
@@ -17,25 +18,43 @@ source("4_weather_interpolation.R") # to retrieve temp&rainfall interpolation
 # grp by planning area, get avg of price
 
 hdb_info_across_planning_area <- readOGR("data/hdb_info_across_planning_area")
+hdb_info_across_planning_area@data[is.na(hdb_info_across_planning_area@data)] = 0 # replace na values on floor with 0
 sf_hdb_info_across_planning_area <- st_make_valid(st_as_sf(hdb_info_across_planning_area))
 sf_hdb_info_across_planning_area <- st_transform(st_as_sf(hdb_info_across_planning_area), crs=3414)
-head(hdb_info_across_planning_area)
+#head(sf_hdb_info_across_planning_area)
 
 hdb_skyrise.csr <- readOGR("data/skyrise_hdb")
 hdb_skyrise.csr@coords <- hdb_skyrise.csr@coords[, 1:2] # Drop Z coord
 hdb_skyrise.csr@data <- hdb_skyrise.csr@data[,-(1:32)]
 sf_hdb_skyrise_greenery.csr <- st_as_sf(hdb_skyrise.csr)
-#sf_hdb_skyrise_greenery.csr <- st_transform(hdb_skyrise.csr, crs= 3414)
+sf_hdb_skyrise_greenery.csr <- st_transform(sf_hdb_skyrise_greenery.csr, crs= 3414)
+#head(sf_hdb_skyrise_greenery.csr)
 
 
 ##### ----- rasterize mean HDB floor for hypo test ----- #####
+# EDA Visualisation on map
+tm_shape(sf_hdb_info_across_planning_area) + tm_polygons("floor", title="Mean floor of HDBs") +
+  tm_layout(
+    title = "Mean Floor of HDBs by Planning Area", 
+    title.size = tm_layout.title.size, title.position=tm_layout.title.position, 
+    title.fontface=tm_layout.title.fontface, title.fontfamily=tm_layout.title.fontfamily, 
+    inner.margins=tm_layout.inner.margins
+  ) +  
+  tm_legend(position=c("right", "bottom"), title.size = 0.7, text.size = 0.6) +
+  tm_scale_bar(position=c("right", "bottom")) + 
+  tm_compass(type=tm_compass.type, position=tm_compass.position, 
+             show.labels=tm_compass.show.labels, size=1.5) +
+  tm_shape(sf_planning_area) + tm_fill(col="white", alpha = 0.05) + tm_borders(col="grey", alpha=0.5) 
+
+
 hdb_floor.r <- raster(nrow = 180, ncols = 360, ext = extent(sf_hdb_info_across_planning_area)) 
 hdb_floor.r <- rasterize(hdb_info_across_planning_area, hdb_floor.r, field = "floor")
 
-crs(hdb_floor.r) <- crs(sf_planning_area) 
+crs(hdb_floor.r) <- crs(sf_hdb_info_across_planning_area) 
 
-tm_shape(sf_hdb_info_across_planning_area) + tm_polygons() +
+# Raster visualisation
 tm_shape(hdb_floor.r) + tm_raster(title = "Mean Floor by planning area") 
+
 
 
 ##### ----- Hypo test ----- #####
@@ -45,18 +64,27 @@ skyrise_hdb_ppp <- as.ppp(sf_hdb_skyrise_greenery.csr)
 planning_area.owin <- as.owin(sf_hdb_info_across_planning_area)
 r_temp.im <- as.im(r_temp.m) #from temp interpolation
 r_rainfall.im <- as.im(r_rainfall.m) #from rainfall interpolation
+hdb_floor.im <- as.im(hdb_floor.r)
+
+
+#new: hdb price raster
+hdb_price.im <- as.im("hdb_price raster layer") #to be done
+
 
 # rescale to be based on km
 skyrise_hdb_ppp.km <- rescale(skyrise_hdb_ppp, 1000, "km")
-pop_den.im.km    <- rescale(pop_den.im, 1000, "km")
+pop_den.im.km <- rescale(pop_den.im, 1000, "km")
 planning_area.owin.km <- rescale(planning_area.owin, 1000, "km")
-r_temp.im.km    <- rescale(r_temp.im, 1000, "km")
-r_rainfall.im.km    <- rescale(r_rainfall.im, 1000, "km")
+r_temp.im.km <- rescale(r_temp.im, 1000, "km")
+r_rainfall.im.km <- rescale(r_rainfall.im, 1000, "km")
+hdb_floor.im.km <- rescale(hdb_floor.im, 1000, "km")
 
+#new: hdb price raster
+hdb_price.im.km <- rescale(hdb_price.im, 1000, "km")
 
 
 ann.p <- mean(nndist(skyrise_hdb_ppp.km, k=1)) 
-ann.p
+ann.p #0.3827644km
 
 
 
@@ -68,11 +96,11 @@ for (i in 1:n){
   ann.r[i] <- mean(nndist(rand.p, k=1))
 } 
 
-plot(rand.p, pch=16, main="H0: Skyrise greenery follows CSR", cols=rgb(0,0,0,0.5))
+plot(rand.p, pch=16, main="H0: HDB Skyrise greenery follows CSR", cols=rgb(0,0,0,0.5))
 #Histogram of simulated KNN values
 hist(ann.r, main="H0: Skyrise greenery follows CSR", las=1, breaks=40, col="bisque", xlim=range(ann.p, ann.r))
 abline(v=ann.p, col="blue")
-# Interpretation: For CSR skyrise would be distributed with dis ranging btw 700-800m
+# Interpretation: For CSR skyrise would be distributed with dis ranging btw 1000-1500m
 
 ## Alternative Hypothesis 1 - with the influence of pop density
 ann.r_alt <- vector(length=n) 
@@ -124,6 +152,22 @@ N.greater3 <- sum(ann.r_alt3/1000 > ann.p)
 p3 <- min(N.greater3 + 1, n + 1 - N.greater3) / (n +1) 
 p3
 
+## Alternative Hypothesis 4 - with the hdb height (floors)
+ann.r_alt4 <- vector(length=n) 
+for (i in 1:n){ 
+  rand.p.h4 <- rpoint(n=skyrise_hdb_ppp.km$n, f=hdb_floor.im.km, win=planning_area.owin.km)
+  ann.r_alt4[i] <- mean(nndist(rand.p.h4, k=1)) 
+} 
+Window(rand.p.h4) <- planning_area.owin.km
+plot(rand.p.h4, pch=16, main="H3", cols=rgb(0,0,0,0.5))
+#Histogram of simulated KNN values
+hist(ann.r_alt4, main="Alt Hypothesis (H3): Rainfall", las=1, breaks=40, col="bisque", xlim=range(ann.p, ann.r_alt4))
+abline(v=ann.p, col="blue")
+
+N.greater4 <- sum(ann.r_alt4/1000 > ann.p) 
+p4 <- min(N.greater4 + 1, n + 1 - N.greater4) / (n +1) 
+p4
+
 
 ##### ----- Point Poisson & ANOVA ----- #####
 ppm_h0 <- ppm(skyrise_hdb_ppp.km ~ 1) #H0: not a fn of pop density
@@ -140,7 +184,7 @@ ppm_h2
 ppm_h3 <- ppm(skyrise_hdb_ppp.km ~ pop_den.im.km + r_temp.im.km + r_rainfall.im.km)
 ppm_h3
 
-# h4 - add hdb floors
+# h4 -hdb floors
 ppm_h4 <- ppm(skyrise_hdb_ppp.km ~ pop_den.im.km + r_temp.im.km + r_rainfall.im.km + hdb_floor.im)
 ppm_h4
 
@@ -148,9 +192,9 @@ anova(ppm_h0, ppm_h1, ppm_h2, ppm_h3, ppm_h4, test="LRT")
 
 
 
-#potential h2 - add prices?
-# ppm_h2 <- ppm(skyrise_hdb_ppp.km~pop_den.im.km) #H1
-#ppm_h2
+# h5 - add prices
+# ppm_h5 <- ppm(skyrise_hdb_ppp.km~pop_den.im.km) #H1
+#ppm_h5
 
 
 
--- 6_csr_analysis.R
+++ 6_csr_analysis.R
@@ -1,3 +1,11 @@
+# Load Libraries and Variables
+#===============================================================
+
+# Data Files Required:
+# 1. onemap_subzone (shp folder)
+# 2. skyrise_greenery (shp folder)
+# 3. planning area population for density calculation (csv)
+
 library(rgdal) 
 library(maptools) 
 library(raster)
@@ -9,11 +17,15 @@ library(raster)
 library(units) # change area units to km
 
 source("4_weather_interpolation.R") # to retrieve temp&rainfall interpolation
+source("constants.R")
 
-##### ----- upload data & manipulate it to have required info ----- ####
+
+# Loading and Preparing Data
+#===============================================================
+# Upload data & manipulate it to have required info
 skyrise_greenery.csr <- readOGR("data/skyrise_greenery")
 skyrise_greenery.csr@coords <- skyrise_greenery.csr@coords[, 1:2] # Drop Z coord
-skyrise_greenery.csr@data <- skyrise_greenery.csr@data[,-(1:4)]
+skyrise_greenery.csr@data <- skyrise_greenery.csr@data[,-(1:4)] # Make it as unmarked points
 sf_skyrise_greenery.csr <- st_as_sf(skyrise_greenery.csr)
 sf_skyrise_greenery.csr <- st_transform(sf_skyrise_greenery.csr, crs= 3414)
 
@@ -31,34 +43,54 @@ sf_planning_area$area_km <- set_units(sf_planning_area$area, "km^2")
 sf_planning_area$pop_density <- sf_planning_area$Population / sf_planning_area$area_km
 
 
-##### ----- rasterize population density for hypo test ----- #####
+# Rasterize population density for hypothesis test
 pop_den.r <- raster(nrow = 180, ncols = 360, ext = extent(sf_planning_area)) 
 pop_den.r <- rasterize(sf_planning_area, pop_den.r, field = "pop_density")
 
-crs(pop_den.r) <- crs(sf_planning_area) 
-
+crs(pop_den.r) <- crs(sf_planning_area)
 tm_shape(pop_den.r) + tm_raster(title = "Population density by planning area") 
 
 
-##### ----- Hypo test ----- #####
-# converting data for use in hypo test
+# EDA - population density
+#===============================================================
+# Generate choropleth map for population density
+tm_shape(sf_planning_area) + tm_polygons("pop_density", title="Population Density (km^2)") +
+  tm_layout(
+    title = "Population Density by Planning Area", 
+    title.size = tm_layout.title.size, title.position=tm_layout.title.position, 
+    title.fontface=tm_layout.title.fontface, title.fontfamily=tm_layout.title.fontfamily, 
+    inner.margins=tm_layout.inner.margins
+  ) +  
+  tm_legend(position=c("right", "bottom"), title.size = 0.6, text.size = 0.5) +
+  tm_scale_bar(position=c("right", "bottom")) + 
+  tm_compass(type=tm_compass.type, position=tm_compass.position, 
+             show.labels=tm_compass.show.labels, size=tm_compass.size) +
+  tm_shape(sf_planning_area) + tm_fill(col="white", alpha = 0.05) + tm_borders(col="grey", alpha=0.5) 
+
+
+
+# Hypothesis Test 
+#===============================================================
+# Converting data for use in hypo test
 pop_den.im <- as.im(pop_den.r)
 skyrise_ppp <- as.ppp(sf_skyrise_greenery.csr)
 planning_area.owin <- as.owin(sf_planning_area)
 r_temp.im <- as.im(r_temp.m) #from temp interpolation
 r_rainfall.im <- as.im(r_rainfall.m) #from rainfall interpolation
 
-# rescale to be based on km
+# Rescale to be based on km
 skyrise_ppp.km <- rescale(skyrise_ppp, 1000, "km")
 pop_den.im.km    <- rescale(pop_den.im, 1000, "km")
 planning_area.owin.km <- rescale(planning_area.owin, 1000, "km")
 r_temp.im.km    <- rescale(r_temp.im, 1000, "km")
 r_rainfall.im.km    <- rescale(r_rainfall.im, 1000, "km")
 
+
 ann.p <- mean(nndist(skyrise_ppp.km, k=1)) 
 ann.p #avg nn distance is 379.6014m
 
-## Null Hypothesis CSR - randomly located ard SG
+## ---------- Running CSR ------------ ##
+# Null Hypothesis (Base model) - location of skyrise greenery consistent with CSR
 n <- 599L 
 ann.r <- vector(length = n) 
 for (i in 1:n){ 
@@ -67,12 +99,13 @@ for (i in 1:n){
 } 
 
 plot(rand.p, pch=16, main="H0: Skyrise greenery follows CSR", cols=rgb(0,0,0,0.5))
-#Histogram of simulated KNN values
+# Histogram of simulated KNN values
 hist(ann.r, main="H0: Skyrise greenery follows CSR", las=1, breaks=40, col="bisque", xlim=range(ann.p, ann.r))
 abline(v=ann.p, col="blue")
 # Interpretation: For CSR skyrise would be distributed with dis ranging btw 700-800m
 
-## Alternative Hypothesis 1 - with the influence of pop density
+
+# Alternative Hypothesis 1 - with the influence of pop density
 ann.r_alt <- vector(length=n) 
 for (i in 1:n){ 
   rand.p.h1 <- rpoint(n=skyrise_ppp.km$n, f=pop_den.im.km, win=planning_area.owin.km)
@@ -88,6 +121,7 @@ abline(v=ann.p, col="blue")
 N.greater <- sum(ann.r_alt/1000 > ann.p) 
 p <- min(N.greater + 1, n + 1 - N.greater) / (n +1) 
 p #0.001666667
+
 
 ## Alternative Hypothesis 2 - with the influence of temperature
 ann.r_alt2 <- vector(length=n) 
@@ -124,6 +158,7 @@ p3
 
 
 ##### ----- Point Poisson & ANOVA ----- #####
+#===============================================================
 ppm_h0 <- ppm(skyrise_ppp.km ~ 1) #H0: not a fn of pop density
 ppm_h0
 
